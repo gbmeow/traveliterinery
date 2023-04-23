3849463468693808aed49a1a306e1d6d834bdbcf
George Batalinski <george.batalinski@gmail.com>
2023-04-23 18:35:18 -0400

    update itinerary

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ document.getElementById('itinerary-form').addEventListener('submit', function(ev
     const date = document.getElementById('date').value;
     const activity = document.getElementById('activity').value;
 
-    const itineraryItem = document.createElement('li');
-    itineraryItem.innerText = `${destination} (${date}): ${activity}`;
+    // Calculate time allocation for activities
+    const itineraryList = document.getElementById('itinerary-list');
+    const itemCount = itineraryList.childElementCount;
 
-    document.getElementById('itinerary-list').appendChild(itineraryItem);
+    // Check if the activities exceed the time limit (6am to 6pm)
+    if (itemCount >= 6) {
+        alert('You have reached the maximum number of activities for the day. Please remove an activity before adding a new one.');
+        return;
+    }
+
+    // Calculate start and end times for the activity
+    const startTime = new Date(date);
+    startTime.setHours(6 + itemCount * 2, 0, 0);
+    const endTime = new Date(startTime);
+    endTime.setHours(startTime.getHours() + 2);
+
+    const timeFormatOptions = { hour: '2-digit', minute: '2-digit' };
+    const startTimeFormatted = startTime.toLocaleTimeString('en-US', timeFormatOptions);
+    const endTimeFormatted = endTime.toLocaleTimeString('en-US', timeFormatOptions);
+
+    // Create a list item for the activity with allocated time
+    const itineraryItem = document.createElement('li');
+    itineraryItem.innerText = `${destination} (${date}): ${activity} | ${startTimeFormatted} - ${endTimeFormatted}`;
+
+    itineraryList.appendChild(itineraryItem);
 
     // Clear the form fields after submission
     document.getElementById('destination').value = '';
